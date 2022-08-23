d772087062ba694e72b799ac0feb3efb57475c61
greatt8696 <https://github.com/greatt8696>
2022-08-23 11:11:16 +0900

    뀨

--- app2.js
+++ app2.js
@@ -3,7 +3,7 @@ const ejs = require("ejs");
 const path = require("path");
 // 이렇게 폴더 경로까지만 잡으면 index 탐색 찾은 index파일을 가져옴.
 const { sequelize, User, Post, Nft, Rank } = require("./model");
-const { fstat } = require("fs");
+const fs = require("fs");
 const { createUid, createNftId } = require("./util/createRandom");
 
 const app = express();
@@ -16,12 +16,14 @@ const PORT = 3000;
 // // app.set express에 값을 저장가능 밑에 구문은 view키에 주소값을 넣은 부분
 app.set("views", path.join(__dirname, "view"));
 
+
 app.engine("html", ejs.renderFile);
 
 app.set("view engine", "html");
 
-// //body 객체 사용
+//body 객체 사용
 app.use(express.urlencoded({ extended: false }));
+
 
 // // sequelize 구성 연결 및 테이블 생성 여기가 처음 매핑
 // // sync 함수는 데이터베이스 동기화하고 필요한 테이블을 생성해준다.
@@ -33,52 +35,16 @@ sequelize
   .sync({ force: true })
   .then(() => {
     console.log("DB연결 성공");
-
-    User.create({
-      uid: createUid(),
-      pwd: "쀼쀼쀼쀼쀼",
-      name: "name",
-      email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
-      balance: 987654321098765,
-      grade: 0,
-      gallery: JSON.stringify([
-        createUid(),
-        createUid(),
-        createUid(),
-        createUid(),
-      ]),
-    });
-
-    Nft.create({
-      nft_id: createNftId(),
-      title: "쀼쀼쀼쀼쀼",
-      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
-      img_url: "/뀨쀼뀨쀼뀨.",
-      history: JSON.stringify([
-        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
-        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
-        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
-        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
-        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
-      ]),
-    });
-
-    Rank.create({
-      nft_id: createNftId(),
-      score: 1231231231,
-      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
-      img_url: "/뀨쀼뀨쀼뀨.",
-      user_id: createUid(),
-    });
+    initDbMultiple();
   })
 
   .catch((err) => {
     console.log(err);
   });
 
-// app.get("/", (req, res) => {
-//   res.render("create");
-// });
+app.get("/", (req, res) => {
+  res.render("index");
+});
 // app.post("/create", (req, res) => {
 //   // create이 함수를 사용하면 해당 테이블에 컬럼을 추가할 수 이다.
 //   const { name, age, msg } = req.body;
@@ -161,6 +127,354 @@ sequelize
 //   Post.update({ msg }, { where: { id, msg: text } });
 // });
 
-// app.listen(PORT, () => {
-//   console.log(PORT, "번 포트 열림");
-// });
+app.listen(PORT, () => {
+  console.log(PORT, "번 포트 열림");
+});
+
+function createOne() {
+  User.create({
+    uid: createUid(),
+    pwd: "쀼쀼쀼쀼쀼",
+    name: "name",
+    email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
+    balance: 987654321098765,
+    grade: 0,
+    gallery: JSON.stringify([
+      createUid(),
+      createUid(),
+      createUid(),
+      createUid(),
+    ]),
+  });
+
+  Nft.create({
+    nft_id: createNftId(),
+    title: "쀼쀼쀼쀼쀼",
+    content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+    img_url: "/뀨쀼뀨쀼뀨.",
+    history: JSON.stringify([
+      { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+      { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+      { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+      { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+      { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+    ]),
+  });
+
+  Rank.create({
+    nft_id: createNftId(),
+    score: 1231231231,
+    nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+    img_url: "/뀨쀼뀨쀼뀨.",
+    user_id: createUid(),
+  });
+}
+
+function initDbMultiple() {
+  User.bulkCreate([
+    {
+      uid: createUid(),
+      pwd: "쀼쀼쀼쀼쀼",
+      name: "name",
+      email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
+      balance: 987654321098765,
+      grade: 0,
+      gallery: JSON.stringify([
+        createUid(),
+        createUid(),
+        createUid(),
+        createUid(),
+      ]),
+    },
+    {
+      uid: createUid(),
+      pwd: "쀼쀼쀼쀼쀼",
+      name: "name",
+      email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
+      balance: 987654321098765,
+      grade: 0,
+      gallery: JSON.stringify([
+        createUid(),
+        createUid(),
+        createUid(),
+        createUid(),
+      ]),
+    },
+    {
+      uid: createUid(),
+      pwd: "쀼쀼쀼쀼쀼",
+      name: "name",
+      email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
+      balance: 987654321098765,
+      grade: 0,
+      gallery: JSON.stringify([
+        createUid(),
+        createUid(),
+        createUid(),
+        createUid(),
+      ]),
+    },
+    {
+      uid: createUid(),
+      pwd: "쀼쀼쀼쀼쀼",
+      name: "name",
+      email: "뀨뀨뀨뀨뀨뀨뀨@naver.com",
+      balance: 987654321098765,
+      grade: 0,
+      gallery: JSON.stringify([
+        createUid(),
+        createUid(),
+        createUid(),
+        createUid(),
+      ]),
+    },
+  ]);
+
+  Nft.bulkCreate([
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+    {
+      nft_id: createNftId(),
+      title: "쀼쀼쀼쀼쀼",
+      content: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      history: JSON.stringify([
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 9999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 999999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 99999 },
+        { prev_owner: createUid(), curr_owner: createUid(), price: 10000 },
+      ]),
+    },
+  ]);
+
+  Rank.bulkCreate([
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+    {
+      nft_id: createNftId(),
+      score: 1231231231,
+      nickname: "뀨뀨뀨뀨뀨뀨뀨뀨",
+      img_url: "/뀨쀼뀨쀼뀨.",
+      user_id: createUid(),
+    },
+  ]);
+}
